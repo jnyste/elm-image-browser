01b014daf46e691c950e3cb1264469ba65153290
Jan-Sebastian Nystedt <33163299+jnyste@users.noreply.github.com>
2018-08-31 04:50:07 +0000

    Set up Hello World

---
+++ src/Main.elm
@@ -0,0 +1,66 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Events
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    String
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( "Hello World"
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = None
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        None ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ h1 [] [ text "Hello World!" ] ]
