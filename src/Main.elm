aa01b7dc996ce8fbb7e9b460b99f29ac9d16aa52
Jan-Sebastian Nystedt <33163299+jnyste@users.noreply.github.com>
2018-08-31 09:26:51 +0000

    proof of concept done, added json server

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,13 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
 import Html exposing (..)
 import Html.Events
+import Http
+import Json.Decode as Decode
+import Task
+import Time
+import Url.Builder as Url
 
 
 
@@ -23,14 +28,17 @@ main =
 
 
 type alias Model =
-    String
+    { zone : Time.Zone, time : Time.Posix, content : List String }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( "Hello World"
-    , Cmd.none
-    )
+    ( Model Time.utc (Time.millisToPosix 0) [ "Hello World" ], Task.perform AdjustTimeZone Time.here )
+
+
+fileDecoder : Decode.Decoder (List String)
+fileDecoder =
+    Decode.field "files" (Decode.list Decode.string)
 
 
 
@@ -38,23 +46,63 @@ init _ =
 
 
 type Msg
-    = None
+    = RefreshFiles
+    | FilesRefreshed (Result Http.Error (List String))
+    | AdjustTimeZone Time.Zone
+    | Tick Time.Posix
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        None ->
+        RefreshFiles ->
             ( model, Cmd.none )
+
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
+
+        Tick newTime ->
+            ( { model | time = newTime }
+            , refreshFiles
+            )
+
+        FilesRefreshed result ->
+            case result of
+                Ok files ->
+                    ( { model | content = files }, Cmd.none )
+
+                Err error ->
+                    case error of
+                        Http.Timeout ->
+                            ( { model | content = [ "Timeout" ] }, Cmd.none )
+
+                        Http.NetworkError ->
+                            ( { model | content = [ "Network Error" ] }, Cmd.none )
+
+                        Http.BadStatus pl ->
+                            ( { model | content = [ "PL" ] }, Cmd.none )
+
+                        Http.BadPayload s s2 ->
+                            ( { model | content = [ s2.body ] }, Cmd.none )
+
+                        Http.BadUrl _ ->
+                            ( { model | content = [ "URL" ] }, Cmd.none )
 
 
 
 -- SUBSCRIPTIONS
 
 
+refreshFiles : Cmd Msg
+refreshFiles =
+    Http.send FilesRefreshed (Http.get "http://37.139.3.80:3000/files" fileDecoder)
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Time.every 1000 Tick
 
 
 
@@ -63,4 +111,4 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ h1 [] [ text "Hello World!" ] ]
+    div [] [ h1 [] [ text (String.concat model.content) ] ]
